580583b5edc9e143425106c39793931c9c42eb71
Draemiz <draemizgx1@gmail.com>
2026-05-09 20:15:05 -0500

    AMM.Subiendo examen práctico Alexandra Moreno con un primer commit de creacion de proyecto inicial

---
+++ src/main/java/com/krakedev/examen/ExamenAlexandraMorenoApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAlexandraMorenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenAlexandraMorenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakedev/examenamm/ExamenAlexandraMorenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.examenamm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAlexandraMorenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
