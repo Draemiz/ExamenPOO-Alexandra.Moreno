7124659237ba1edbc509755196bb9363be962200
Draemiz <draemizgx1@gmail.com>
2026-05-09 21:03:02 -0500

    AMM. prueba unitaria de TestVendedor, para calcular sueldo realizada con exito

---
+++ src/test/java/com/krakedev/examen/TestJUnit/VendedorTestJUnit.java
@@ -0,0 +1,23 @@
+package com.krakedev.examen.TestJUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.examen.dto.Vendedor;
+
+public class VendedorTestJUnit {
+	
+	@Test
+    public void testCalcularSueldo() {
+
+        Vendedor vendedor = new Vendedor("123");
+
+        vendedor.setSueldoFijo(500);
+
+        double esperado = 500;
+        double actual = vendedor.calcularSueldo();
+
+        assertEquals(esperado, actual);
+    }
+
+}
